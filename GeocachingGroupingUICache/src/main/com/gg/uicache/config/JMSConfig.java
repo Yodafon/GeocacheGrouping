712b7282e624972867b052b95fc3b1beb2711ec3
László Kovács <yodafon@freemail.hu>
2023-09-04 13:50:15 +0200

    Add constant

--- GeocachingGroupingUICache/src/main/com/gg/uicache/config/JMSConfig.java
+++ GeocachingGroupingUICache/src/main/com/gg/uicache/config/JMSConfig.java
@@ -9,11 +9,13 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JMSConfig {
 
+    public static final String JMS_MESSAGE_TYPE_ID = "_type";
+
     @Bean // Serialize message content to json using TextMessage
     public MessageConverter jacksonJmsMessageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
-        converter.setTypeIdPropertyName("_type");
+        converter.setTypeIdPropertyName(JMS_MESSAGE_TYPE_ID);
         return converter;
     }
 
