716ff1c2ee7e2ea3199fcabb804c87d52a949991
László Kovács <yodafon@freemail.hu>
2023-11-06 23:59:01 +0100

    Moved AppStart main class from config to core package

--- GeocachingGrouping/src/com/gg/core/AppStart.java
+++ GeocachingGrouping/src/com/gg/core/AppStart.java
@@ -1,15 +1,18 @@
-package com.gg.core.config;
+package com.gg.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan("com.gg.core")
 public class AppStart {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppStart.class);
+
     public static void main(String[] args) {
+        System.out.println("Starting Grouping application");
         SpringApplication.run(AppStart.class, args);
     }
 
