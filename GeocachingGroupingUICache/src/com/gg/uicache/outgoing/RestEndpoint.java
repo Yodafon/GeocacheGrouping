3c21d435cf7b4844ad0328ed80b7b2d3aaad5ea4
László Kovács <yodafon@freemail.hu>
2023-12-06 14:35:25 +0100

    Add "Gradle test" test step to run tests

--- GeocachingGroupingUICache/src/com/gg/uicache/outgoing/RestEndpoint.java
+++ GeocachingGroupingUICache/src/com/gg/uicache/outgoing/RestEndpoint.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
-@RestController()
+@RestController
 @RequestMapping("/uicache")
 @CrossOrigin
 public class RestEndpoint {
